4528d7e9eefb25837b817f25f3c0f8615126d4e1
Alan Mitchell <tabb99@gmail.com>
2021-12-13 11:44:37 -0900

    Put SQL files into queries/ directory.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sqlite3 -header -csv bmon.sqlite < sensors.sql > data/sensors.csv
-sqlite3 -header -csv bmon.sqlite < alerts.sql > data/alerts.csv
+sqlite3 -header -csv bmon.sqlite < queries/sensors.sql > data/sensors.csv
+sqlite3 -header -csv bmon.sqlite < queries/alerts.sql > data/alerts.csv
