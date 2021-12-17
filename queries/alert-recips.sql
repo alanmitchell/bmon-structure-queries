65e6dee6fa290b0591fafa6456d3d6e5049b0c88
Alan Mitchell <tabb99@gmail.com>
2021-12-16 18:51:39 -0900

    Added alert query w/o recipients.

--- queries/alert-recips.sql
+++ queries/alert-recips.sql
@@ -2,17 +2,22 @@ SELECT
 	building.title AS building_name,
 	sensor.title AS sensor_name,
 	alert.condition AS alert_condition,
-	alert.test_value AS test_value
+	alert.test_value AS test_value,
+	recipient.name AS person_alerted
 FROM 
 	bmsapp_sensor AS sensor,  
 	bmsapp_building AS building,
 	bmsapp_alertcondition AS alert,
+	bmsapp_alertrecipient AS recipient,
+	bmsapp_alertcondition_recipients AS alert_recipients,
 	bmsapp_bldgtosensor AS bldg_to_sensor
 WHERE
 	building.id = bldg_to_sensor.building_id AND
 	sensor.id = bldg_to_sensor.sensor_id AND
+	alert.id = alert_recipients.alertcondition_id AND
+	recipient.id = alert_recipients.alertrecipient_id AND 
 	sensor.id = alert.sensor_id AND
 	alert.active = 1
 ORDER BY
-	building_name, bldg_to_sensor.sort_order
+	building_name, bldg_to_sensor.sort_order, person_alerted
 ;
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sqlite3 -header -csv bmon.sqlite < queries/sensors.sql > data/sensors.csv
+sqlite3 -header -csv bmon.sqlite < queries/alert-recips.sql > data/alert-recips.csv
 sqlite3 -header -csv bmon.sqlite < queries/alerts.sql > data/alerts.csv
